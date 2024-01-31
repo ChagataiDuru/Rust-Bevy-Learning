e3b85e165b3b423091d99df2c246f323b5f5d6d4
Çağatay <durucagatay01@gmail.com>
2024-01-31 16:45:20 +0300

    Simple Camera and Input system

--- simple-platformer/src/main.rs
+++ simple-platformer/src/main.rs
@@ -2,10 +2,34 @@ use bevy::prelude::*;
 
 fn main() {
     App::new()
-    .add_systems(Update, hello_world_system)
-    .run();
+        .insert_resource(ClearColor(Color::rgb(0.5, 0.5, 0.9)))
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .add_systems(Update, (keyboard_input_system, change_clear_color))
+        .run();
 }
 
-fn hello_world_system() {
-    println!("hello world");
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
+}
+
+fn change_clear_color(input: Res<Input<KeyCode>>, mut clear_color: ResMut<ClearColor>) {
+    if input.just_pressed(KeyCode::Space) {
+        clear_color.0 = Color::PURPLE;
+    }
+}
+
+/// This system prints 'A' key state
+fn keyboard_input_system(keyboard_input: Res<Input<KeyCode>>) {
+    if keyboard_input.pressed(KeyCode::A) {
+        info!("'A' currently pressed");
+    }
+
+    if keyboard_input.just_pressed(KeyCode::A) {
+        info!("'A' just pressed");
+    }
+
+    if keyboard_input.just_released(KeyCode::A) {
+        info!("'A' just released");
+    }
 }
