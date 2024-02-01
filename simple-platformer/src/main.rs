d866cc37bca620e82d8edf6ffcc68d1ca7714c9e
Çağatay <durucagatay01@gmail.com>
2024-02-01 15:40:42 +0300

    Simple entity system ex: space ship

--- simple-platformer/src/main.rs
+++ simple-platformer/src/main.rs
@@ -1,16 +1,46 @@
 use bevy::prelude::*;
 
+#[derive(Component, Debug)]
+struct Position {
+    x: f32,
+    y: f32,
+}
+
+#[derive(Component, Debug)]
+struct Velocity {
+    x: f32,
+    y: f32,
+}
+
+// Need a Hashmap that holds some predifined Colors such as Purple,Color::Purple
+
+
 fn main() {
     App::new()
         .insert_resource(ClearColor(Color::rgb(0.5, 0.5, 0.9)))
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup)
-        .add_systems(Update, (keyboard_input_system, change_clear_color))
+        .add_systems(Update, (keyboard_input_system, change_clear_color,update_position, print_position))
         .run();
 }
 
 fn setup(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
+    commands.spawn((Position { x: 0.0, y: 0.0 }, Velocity { x: 1.0, y: 1.0 }));
+}
+
+fn update_position(mut query: Query<(&Velocity, &mut Position)>) {
+    for (velocity, mut position) in query.iter_mut() {
+        position.x += velocity.x;
+        position.y += velocity.y;
+    }
+}
+
+fn print_position(query: Query<(Entity, &Position)>) {
+    // Log the entity ID and position of each entity with a `Position` component.
+    for (entity, position) in query.iter() {
+        info!("Entity {:?} is at position {:?},", entity, position);
+    }
 }
 
 fn change_clear_color(input: Res<Input<KeyCode>>, mut clear_color: ResMut<ClearColor>) {
